6fe3f386bb9fc20992160951dbc3de2a63847217
ivanminutillo <ivanminutillo@outlook.it>
2022-06-01 17:17:30 +0200

    ui

--- lib/components/modals/reusable_modal_live.ex
+++ lib/components/modals/reusable_modal_live.ex
@@ -8,13 +8,13 @@ defmodule Bonfire.UI.Common.ReusableModalLive do
   prop title_text, :string
 
   @doc "The classes of the title of the modal"
-  prop title_class, :string, default: "font-bold text-lg"
+  prop title_class, :string, default: "font-bold text-base"
 
   @doc "The classes of the open button for the modal. Only used if no open_btn slot is passed."
-  prop open_btn_class, :string, default: "btn btn-primary"
+  prop open_btn_class, :string, default: "btn btn-primary btn-sm normal-case"
 
   @doc "The classes of the close/cancel button on the modal. Only used if no close_btn slot is passed."
-  prop cancel_btn_class, :string, default: "btn btn-outline"
+  prop cancel_btn_class, :string, default: "btn btn-outline btn-sm normal-case"
 
   @doc "Force modal to be open"
   prop show, :boolean, default: false
