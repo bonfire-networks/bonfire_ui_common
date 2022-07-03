9edfb757d4f3ccc6f2651ff0601e6946175befaa
Mayel de Borniol <git@mayel.space>
2022-07-04 11:00:13 +1200

    Move ProfileItemLive

---
+++ lib/components/profile_item/profile_item_live.ex
@@ -0,0 +1,10 @@
+defmodule Bonfire.UI.Common.ProfileItemLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop profile, :any
+  prop character, :any
+  prop class, :string
+  prop show_controls, :list, default: [:follow]
+
+  slot default, required: false
+end
