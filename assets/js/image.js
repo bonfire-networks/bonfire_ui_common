a5dc4ded84bef318355d1b29e74b6ba25e0e255e
Mayel de Borniol <git@mayel.space>
2022-07-04 21:22:52 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/408

---
+++ assets/js/image.js
@@ -0,0 +1,50 @@
+let ImageHooks = {};
+
+import exifr from 'exifr'
+
+// async function imageParseMeta(image) {
+//   let options = {
+//     // setting false on these blocks does not read them at all, except for IFD0 which is necessary
+//     // because it contains pointer to GPS IFD. Though no tag values are read and once GPS pointer
+//     // is found the IFD0 search-through ends.
+//     ifd0: false, // image
+//     ifd1: false, // thumbnail
+//     exif: true, // ['ISO', 'FNumber']
+//     iptc: ['Caption', 'Sublocation', 'City', 'State', 'CountryCode', 'Country', 'LocalCaption', 'DocumentNotes', 'Contact', 'Headline', 'Credit', 'Source', 'CopyrightNotice', 'Keywords'],
+//     jfif: false, // (jpeg only)
+//     ihdr: false, // (png only)
+//     tiff: ['UserComment', 'OwnerName', 'SerialNumber'],
+//     xmp: true, // ['author', 'description'],
+//     icc: ['DeviceManufacturer', 'DeviceModel', 'DeviceModelDesc', 'MakeAndModel'],
+//     userComment: true,
+//     // Instead of `true` you can use array of tags to read. All other tags are not read at all.
+//     // You can use string tag names as well as their numeric code. In this example 0x0004 = GPSLongitude
+//     gps: ['GPSLatitudeRef', 'GPSLatitude', 'GPSLongitudeRef', 'GPSLongitude', 'GPSAltitudeRef', 'GPSAltitude', 0x0003, 0x0004],
+//     interop: false,
+//     translateKeys: true,
+//     translateValues: true,
+//     reviveValues: true,
+//     sanitize: true,
+//     mergeOutput: true,
+//     silentErrors: true
+//   }
+//   // let data = await exifr.parse(image, options)
+//   let data = await exifr.parse(image)
+//   console.log(data)
+//   // raw values
+//   return data;
+// }
+
+ImageHooks.imageMetadata = {
+  mounted() {
+    // let img = document.getElementById(this.el.dataset.img)
+    // if (img) img.addEventListener("load", async e => {
+    //   let file = e.target.currentSrc || e.target.src;
+    //   console.log(file)
+    //   imageParseMeta(file).then(ret => console.log('Ret:', ret))
+    // })
+
+  }
+}
+
+export { ImageHooks }
