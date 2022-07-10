550fd946033bf0e0ac5d3d51835555c740b866cb
ivanminutillo <ivanminutillo@outlook.it>
2022-07-10 19:30:17 +0200

    smart input and emoji picker on boundary settings

---
+++ assets/js/emoji.js
@@ -0,0 +1,27 @@
+let EmojiHooks = {};
+
+// import { Picker as Mart } from 'emoji-mart'
+
+// EmojiHooks.EmojiPicker = {
+
+//     mounted() {
+//         const boundary_picker = new Mart({
+//           emojiButtonSize: 30,
+//           emojiSize: 20,
+//           previewPosition: 'none',
+//           data: async () => {
+//             const response = await fetch(
+//               'https://cdn.jsdelivr.net/npm/@emoji-mart/data',
+//             )
+        
+//             return response.json()
+//           }
+//         })
+
+//         document.querySelector('#boundary-picker').appendChild(boundary_picker)
+
+//     },
+
+// }
+
+export { EmojiHooks }
