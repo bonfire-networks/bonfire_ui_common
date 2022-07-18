01c89df80a45a3ebb46c2cca33b29f3b621c82bc
Mayel de Borniol <git@mayel.space>
2022-07-18 14:18:35 +1200

    Add settings

---
+++ lib/components/settings/avatars_settings_live.ex
@@ -0,0 +1,5 @@
+defmodule Bonfire.UI.Common.AvatarsSettingsLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop scope, :atom, default: nil
+end
---
+++ lib/components/settings/smart_input_settings_live.ex
@@ -0,0 +1,6 @@
+defmodule Bonfire.UI.Common.SmartInputSettingsLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop scope, :atom, default: nil
+
+end
