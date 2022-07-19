3ab04193b21fbfb0772a8ce2d05dcb039f385c52
Mayel de Borniol <git@mayel.space>
2022-07-19 20:17:30 +1200

    nav

--- lib/components/nav/sidebar_navigation_live.ex
+++ lib/components/nav/sidebar_navigation_live.ex
@@ -2,6 +2,5 @@ defmodule Bonfire.UI.Common.SidebarNavigationLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop page, :string, required: true
-  prop hide_smart_input, :boolean
-  
+
 end
