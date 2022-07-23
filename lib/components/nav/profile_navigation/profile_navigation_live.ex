6c318ca8a2b9835a833b17a2171c85f30e7558f4
Mayel de Borniol <git@mayel.space>
2022-07-23 14:03:36 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/421

--- lib/components/nav/profile_navigation/profile_navigation_live.ex
+++ lib/components/nav/profile_navigation/profile_navigation_live.ex
@@ -1,6 +1,7 @@
 defmodule Bonfire.UI.Common.ProfileNavigationLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop selected_tab, :string
-  prop user, :map
+  prop selected_tab, :string, default: nil
+  prop path_prefix, :string, required: true
+  prop tabs, :list, required: true
 end
