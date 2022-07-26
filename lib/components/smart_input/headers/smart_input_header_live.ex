228c31390318311eaea85da3f8f28e34dcbfc783
ivanminutillo <ivanminutillo@outlook.it>
2022-07-26 15:41:41 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/429

--- lib/components/smart_input/headers/smart_input_header_live.ex
+++ lib/components/smart_input/headers/smart_input_header_live.ex
@@ -1,26 +1,9 @@
-defmodule Bonfire.UI.Common.SmartInputContainerLive do
+defmodule Bonfire.UI.Common.SmartInputHeaderLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop reply_to_id, :string, default: ""
-  prop thread_id, :string, default: "", required: false
-  prop create_activity_type, :any, default: nil
-  prop smart_input_component, :atom, default: nil
-  prop to_boundaries, :list, default: []
-  prop to_circles, :list, default: nil
   prop smart_input_prompt, :string, required: false
-  prop smart_input_text, :string, required: false
-  prop showing_within, :any, default: nil
-  prop with_rich_editor, :boolean, default: true, required: false
-  prop activity, :any, default: nil
-  prop hide_smart_input, :boolean, default: false
-  prop object, :any, default: nil
-  prop activity_inception, :any, default: nil
-  prop title_open, :boolean, default: false
-  prop title_prompt, :string, default: nil
-  prop preloaded_recipients, :list, default: nil
-  prop show_select_recipients, :boolean, default: false
-  prop thread_mode, :atom, default: nil
-  prop page, :any, default: nil
+  prop smart_input_component, :atom, default: nil
+  prop create_activity_type, :any, default: nil
 
   def all_smart_input_components do
     Bonfire.Common.Config.get([:ui, :smart_input_components], [post: Bonfire.UI.Social.WritePostContentLive])
@@ -47,6 +30,5 @@ defmodule Bonfire.UI.Common.SmartInputContainerLive do
     socket
     |> push_event("smart_input:set_body", %{text: text})
   end
-
 
 end
