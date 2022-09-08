f03adad544621144316667639e51d603e4f645bc
Mayel de Borniol <git@mayel.space>
2022-09-08 23:26:04 +1200

    declare_widget & declare_nav_component

--- lib/components/widgets/widget_feedbacks/feedback_button_live.ex
+++ lib/components/widgets/widget_feedbacks/feedback_button_live.ex
@@ -1,6 +1,13 @@
 defmodule Bonfire.UI.Common.FeedbackButtonLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  @doc "Pre-fill the feedback form with some text"
   prop text, :string, default: nil
+
+  @doc "Classes to style the link/button"
   prop class, :css_class, default: "w-full mt-2 normal-case btn btn-sm btn-info btn-wide"
+
+  import Bonfire.Common.Modularity.DeclareExtensions
+  declare_nav_component("Open to composer ready to provide feedback")
+
 end
--- lib/components/widgets/widget_instance_info/widget_instance_info_live.ex
+++ lib/components/widgets/widget_instance_info/widget_instance_info_live.ex
@@ -1,5 +1,9 @@
 defmodule Bonfire.UI.Common.WidgetInstanceInfoLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  @doc "Show a large instance or app icon?"
   prop display_banner, :boolean, default: true
+
+  import Bonfire.Common.Modularity.DeclareExtensions
+  declare_widget("Instance Details")
 end
