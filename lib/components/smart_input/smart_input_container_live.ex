89844a89efb3b9f4862724739d3ab2cfd2865cc8
ivanminutillo <ivanminutillo@outlook.it>
2022-09-19 15:46:20 +0200

    ui

--- lib/components/smart_input/smart_input_container_live.ex
+++ lib/components/smart_input/smart_input_container_live.ex
@@ -23,7 +23,7 @@ defmodule Bonfire.UI.Common.SmartInputContainerLive do
   prop page, :any, default: nil
   prop without_sidebar, :string, default: nil
 
-  def set_smart_input_as(:flat, _), do: :sidebar
+  def set_smart_input_as(:flat, _), do: :modal
 
   def set_smart_input_as(_, context),
     do: Settings.get([:ui, :smart_input_as], :floating, context)
