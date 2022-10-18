68f98156640947057260dce6eff44e470ae7a59d
Mayel de Borniol <git@mayel.space>
2022-10-19 08:07:42 +1300

    f

--- lib/components/instance_pinned/instance_pinned_live.ex
+++ lib/components/instance_pinned/instance_pinned_live.ex
@@ -1,3 +1,7 @@
 defmodule Bonfire.UI.Common.InstancePinnedLive do
   use Bonfire.UI.Common.Web, :stateless_component
+
+  prop title, :string, default: nil
+  prop object_types, :any, default: []
+  prop load_pointer, :boolean, default: false
 end
