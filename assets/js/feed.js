f26f1c879050f9c4ef8aa9566fefd168984951be
Mayel de Borniol <git@mayel.space>
2022-12-10 12:02:59 +1300

    add an event listener to PreviewContent when the user click on the browser back button to close the thread

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -4,11 +4,11 @@ FeedHooks.PreviewActivity = {
   mounted() {
     this.el.addEventListener("click", e => {
       e.preventDefault(); // TODO: find a better way to hook a simple event on an anchor without needing a hook
+      console.log("PreviewActivity clicked")
       uri = this.el.dataset.permalink
       // this.pushEvent("Bonfire.Social.Feeds:open_activity", { id: this.el.dataset.id, permalink: uri })
       const layout = document.getElementById("root")
       const main = document.getElementById("inner")
-      console.log("inner")
       main.classList.add("hidden")
       let previous_scroll = null
       if (layout) {
@@ -28,24 +28,36 @@ FeedHooks.PreviewActivity = {
 
 FeedHooks.ClosePreview = {
   mounted() {
-    this.el.addEventListener("click", e => {
+
+    const back = function () {
       const layout = document.getElementById("root")
       const main = document.getElementById("inner")
-      console.log("TEST")
+      console.log("attempt going back")
       location_before_preview = history.state["previous_url"]
       previous_scroll = history.state["previous_scroll"]
       main.classList.remove("hidden")
+      console.log(location_before_preview)
       if (location_before_preview) {
-        console.log("qui")
-        console.log(location_before_preview)
         history.pushState({}, '', location_before_preview)
       }
       console.log(previous_scroll)
       if (previous_scroll) {
-        console.log("qui2")
-        layout.scrollTo({top: previous_scroll, behavior: 'instant'})
+        layout.scrollTo({ top: previous_scroll, behavior: 'instant' })
         // window.scrollTo(0, previous_scroll);
       }
+    }
+
+    // close button
+    this.el.addEventListener("click", e => {
+      back()
+    })
+
+    // intercept browser "back" action
+    window.addEventListener("popstate", e => {
+      console.log(e)
+      // e.preventDefault();
+      this.pushEvent("Bonfire.UI.Common.OpenPreviewLive:close", {})
+      back();
     })
   }
 }
