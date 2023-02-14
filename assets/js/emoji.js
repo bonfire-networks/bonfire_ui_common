e68a3624bd61d191c0508fbc017b00f9f5d51d17
ivanminutillo <ivanminutillo@outlook.it>
2023-02-14 21:50:46 +0100

    smart input

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -1,43 +1,43 @@
 let EmojiHooks = {};
 
-import { Picker } from "emoji-mart";
+const { Picker } = await import('emoji-mart')
 import insertText from 'insert-text-at-cursor';
-import data from '@emoji-mart/data'
+const promise = import('@emoji-mart/data/sets/14/twitter.json').then(r => r.default)
 
-const picker = new Picker({
-  data: data,
-  emojiButtonSize: 30,
-  emojiSize: 20,
-  previewPosition: "none",
-  onEmojiSelect: function (emoji) {
-    
-    // Insert the emoji at the cursor position
-    const area = document.querySelector(".composer");
-    
-    // if area is not focused, focus it
-    if (!area.matches(":focus")) {
-      area.focus();
-    }
 
-    insertText(area, emoji.native + " ")
 
-    // close the emojipicker adding style="display: none;"
-    // document.querySelector(".emoji-picker").setAttribute("style", "display: none;")
-  }
-  
-});
+
 EmojiHooks.EmojiPicker = {
-
-    mounted() {
-      // wait 1 second and append the picker to the DOM
-      this.el.querySelector("#picker").appendChild(picker);
+  
+  mounted() {
+    const picker = new Picker({
+      data: () => promise,
+      emojiButtonSize: 30,
+      emojiSize: 20,
+      set: "twitter",
+      previewPosition: "none",
+      onEmojiSelect: function (emoji) {
+        
+        // Insert the emoji at the cursor position
+        const area = document.querySelector(".composer");
+        
+        // if area is not focused, focus it
+        if (!area.matches(":focus")) {
+          area.focus();
+        }
     
-    },
-    updated() {
-      console.log("emoji updated");
-      // load the emoji picker
-    },
+        insertText(area, emoji.native + " ")
+    
+        // close the emojipicker adding style="display: none;"
+        // document.querySelector(".emoji-picker").setAttribute("style", "display: none;")
+      }
+      
+    });
+    // wait for the data to be loaded correctly before appending the picker
+    this.el.querySelector("#picker").appendChild(picker);
 
+    
+    }
 }
 
 export { EmojiHooks }
