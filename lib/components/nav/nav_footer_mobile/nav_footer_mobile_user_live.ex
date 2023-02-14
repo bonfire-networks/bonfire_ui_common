98fa4d0f70446bdfab86c2b505fe1d4a9399b4d1
Mayel de Borniol <git@mayel.space>
2023-02-14 18:09:00 +1300

    f

--- lib/components/nav/nav_footer_mobile/nav_footer_mobile_user_live.ex
+++ lib/components/nav/nav_footer_mobile/nav_footer_mobile_user_live.ex
@@ -1,6 +1,5 @@
 defmodule Bonfire.UI.Common.NavFooterMobileUserLive do
   use Bonfire.UI.Common.Web, :stateless_component
-  alias Bonfire.UI.Common.BadgeCounterLive
 
   prop page, :string
   prop without_sidebar, :boolean, default: false
