cabb8b94b16cc4be4466c7bb8a03161357b66168
Mayel de Borniol <git@mayel.space>
2023-02-25 09:27:14 +1300

    blurHash

--- lib/components/images/lazy_image.ex
+++ lib/components/images/lazy_image.ex
@@ -1,8 +1,9 @@
 defmodule Bonfire.UI.Common.LazyImage do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop src, :string
+  prop media, :any, default: nil
+  prop src, :string, default: nil
+  prop alt, :string, default: nil
   prop class, :css_class, default: nil
-  prop alt, :string
   prop opts, :any, default: %{}
 end
