3defd5ab4d310c82557067b1226577fe3b3efeb8
ivanminutillo <ivanminutillo@outlook.it>
2023-03-07 22:33:04 +0100

    ui

--- lib/components/nav/tabs/tabs_live.ex
+++ lib/components/nav/tabs/tabs_live.ex
@@ -8,16 +8,8 @@ defmodule Bonfire.UI.Common.TabsLive do
 
   prop link_component, :atom, default: LinkPatchLive
 
-<<<<<<< HEAD
-  prop class, :css_class, default: "flex gap-3 pt-1 md:pt-4 p-3 px-4 bg-base-100"
-
-  prop tab_class, :css_class,
-    default:
-      "btn hover:bg-base-content hover:!text-base-300 btn-sm !max-h-[1.75rem] !min-h-[1.75rem] !h-[1.75rem] rounded-full font-normal capitalize bg-base-content/10 text-base-content/70 feed_tab border-none"
-=======
   prop class, :css_class
   prop tab_class, :css_class
->>>>>>> 175468d (ui)
 
   prop tab_primary_class, :css_class, default: "btn-primary"
   @doc "What LiveHandler and/or event name to send the patch event to (optional)"
