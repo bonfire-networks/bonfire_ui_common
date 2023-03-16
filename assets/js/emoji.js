071d9351688ab5349ed742e27cc12c4dbc5eae88
ivanminutillo <ivanminutillo@outlook.it>
2023-03-16 15:20:35 +0100

    emojis

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -1,49 +1,45 @@
 let EmojiHooks = {};
 
-import { Picker } from "emoji-mart";
 import insertText from 'insert-text-at-cursor';
-// import data from '@emoji-mart/data'
+import { createPopup } from '@picmo/popup-picker';
+
 
 
 EmojiHooks.EmojiPicker = {
   
   mounted() {
     const target_field = document.querySelector(this.el.dataset.targetField || ".composer");
-    const picker = new Picker({
-      data: async() => {
-        const response = await fetch(
-          'https://cdn.jsdelivr.net/npm/@emoji-mart/data/sets/14/native.json',
-        )
-        return response.json()
-      },
-      emojiButtonSize: 30,
-      emojiSize: 20,
-      previewPosition: "none",
-      set: "native",
-      onEmojiSelect: function (emoji) {
-        
-        if (target_field){ 
+
+    const trigger = document.querySelector('.emoji-button');
+    trigger.addEventListener('click', () => {
+      picker.toggle();
+    });
+
+    const picker = createPopup({}, {
+      referenceElement: trigger,
+      triggerElement: trigger,
+      emojiSize: '1.8rem',
+      className: 'z-[9999]',
+    });
+    
+
+    picker.addEventListener('emoji:select', event => {
+      if (target_field){ 
           // if area is not focused, focus it
           if (!target_field.matches(":focus")) {
             target_field.focus();
           }
       
         // Insert the emoji at the cursor position        
-          insertText(target_field, emoji.native + " ")
+          insertText(target_field, event.emoji + " ")
       
           // close the emojipicker adding style="display: none;"
           // document.querySelector(".emoji-picker").setAttribute("style", "display: none;")
         } else {
             console.log("dunno where to insert the emoji")
         }
-      }
-      
     });
 
-    // wait for the data to be loaded correctly before appending the picker
-    this.el.querySelector(".picker").appendChild(picker);
-
-    
     }
 }
 
