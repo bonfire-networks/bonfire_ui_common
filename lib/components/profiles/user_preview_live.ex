b119a063fc13d7250ac3c17728f1e363931f33bb
Mayel de Borniol <git@mayel.space>
2023-04-03 16:44:50 +1200

    misc

--- lib/components/profiles/user_preview_live.ex
+++ lib/components/profiles/user_preview_live.ex
@@ -1,6 +1,5 @@
 defmodule Bonfire.UI.Common.UserPreviewLive do
   use Bonfire.UI.Common.Web, :stateless_component
-  import Bonfire.UI.Me.Integration, only: [is_admin?: 1]
   import Bonfire.Common.Utils
   # import Bonfire.Common.Media
 
