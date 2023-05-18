3b1e9b24709cf34d61e0433a45d99132b8a1fc73
ivanminutillo <ivanminutillo@outlook.it>
2023-05-18 21:45:50 +0200

    members

--- lib/components/members/members_live.ex
+++ lib/components/members/members_live.ex
@@ -1,5 +1,7 @@
 defmodule Bonfire.UI.Common.MembersLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  prop ghosted_instance_wide?, :boolean, default: nil
+  prop silenced_instance_wide?, :boolean, default: nil
   prop users, :list, required: true
 end
