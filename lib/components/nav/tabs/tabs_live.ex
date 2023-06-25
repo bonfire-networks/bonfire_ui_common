627cd0da629b62f40b6060f56f76dffe3d5139a2
ivanminutillo <ivanminutillo@outlook.it>
2023-06-25 10:45:20 +0200

    ui

--- lib/components/nav/tabs/tabs_live.ex
+++ lib/components/nav/tabs/tabs_live.ex
@@ -10,7 +10,7 @@ defmodule Bonfire.UI.Common.TabsLive do
 
   prop class, :css_class, default: "flex gap-3 pt-1 md:pt-4 p-3 px-4 scrollable"
   prop tab_class, :css_class
-
+  prop item_class, :css_class, default: ""
   prop tab_primary_class, :css_class, default: "btn-primary"
   @doc "What LiveHandler and/or event name to send the patch event to (optional)"
   prop event_handler, :string, default: nil
