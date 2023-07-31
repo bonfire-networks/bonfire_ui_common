a68fc109a32b07ef5089b10c4cc5b26abc5f51f5
ivanminutillo <ivanminutillo@outlook.it>
2023-07-31 15:57:03 +0200

    ui

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -8,9 +8,6 @@ import { createPopup } from '@picmo/popup-picker';
 EmojiHooks.EmojiPicker = {
   
   mounted() {
-    const target_field = document.querySelector(this.el.dataset.targetField || ".composer");
-    console.log(target_field)
-    console.log("target_field")
     const trigger = document.querySelector('.emoji-button');
     trigger.addEventListener('click', () => {
       picker.toggle();
@@ -24,24 +21,27 @@ EmojiHooks.EmojiPicker = {
     });
     
 
-    picker.addEventListener('emoji:select', event => {
-      if (target_field){ 
-          // if area is not focused, focus it
-          if (!target_field.matches(":focus")) {
-            target_field.focus();
-          }
+    // picker.addEventListener('emoji:select', event => {
+    //   const target_field = document.querySelector(this.el.dataset.targetField || ".composer");
+    //   this.pushEvent("emoji-selected", { emoji: event.emoji })
+    //   // if (target_field){ 
+    //   //     // if area is not focused, focus it
+    //   //     if (!target_field.matches(":focus")) {
+    //   //       target_field.focus();
+    //   //     }
       
-        // Insert the emoji at the cursor position        
-          insertText(target_field, event.emoji + " ")
+    //   //   // Insert the emoji at the cursor position        
+    //   //     insertText(target_field, event.emoji + " ")
       
-          // close the emojipicker adding style="display: none;"
-          // document.querySelector(".emoji-picker").setAttribute("style", "display: none;")
-        } else {
-            console.log("dunno where to insert the emoji")
-        }
-    });
+    //   //     // close the emojipicker adding style="display: none;"
+    //   //     // document.querySelector(".emoji-picker").setAttribute("style", "display: none;")
+    //   //   } else {     
+    //   //       console.log("dunno where to insert the emoji")
+    //   //   }
+    // });
 
     }
+
 }
 
 export { EmojiHooks }
