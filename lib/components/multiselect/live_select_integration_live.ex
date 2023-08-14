e00578905437dc44bcd71eb81772b7bff3e389db
Mayel de Borniol <git@mayel.space>
2023-08-14 09:16:50 +0100

    fix

--- lib/components/multiselect/live_select_integration_live.ex
+++ lib/components/multiselect/live_select_integration_live.ex
@@ -25,29 +25,54 @@ defmodule Bonfire.UI.Common.LiveSelectIntegrationLive do
       dropdown_class="cursor-pointer absolute top-12 p-1 z-50 dropdown-content menu shadow w-full bg-base-100 shadow-lg text-base-content rounded"
       tags_container_class="flex flex-wrap gap-1 pt-2 order-last"
     >
-      <:tag :let={option}>
-        <!-- if (debug(@type) || e(debug(option.value), :type, nil)) in [:user, "user"] do -->
-        <%= if Map.has_key?(debug(option.value), :icon) or Map.has_key?(option.value, "icon") do %>
-          <div class="flex gap-2 items-center">
-            <div class="w-8 h-8">
-              <img
-                src={e(option.value, :icon, nil)}
-                alt={e(option.value, :username, nil)}
-                class="w-full h-full rounded-full"
-              />
-            </div>
+      <:option :let={option}>
+        <div class="flex gap-2 items-center">
+          <%= if is_binary(option.value) do %>
+            <p class="font-semibold text-base-content/70">
+              <%= option.label %>
+            </p>
+          <% else %>
+            <%= if Map.has_key?(debug(option).value, :icon) or Map.has_key?(option.value, "icon") do %>
+              <div class="w-8 h-8">
+                <img src={e(option.value, :icon, nil)} alt="" class="w-full h-full rounded-full" />
+              </div>
+            <% end %>
             <div class="">
-              <p class="font-semibold"><%= e(option.value, :name, nil) %></p>
-              <p class="font-light text-base-content/70"><%= e(option.value, :username, nil) %></p>
+              <p class="font-semibold">
+                <%= e(option.value, :name, nil) || e(option.value, :profile, :name, nil) ||
+                  e(option.value, :named, :name, nil) %>
+              </p>
+              <p class="font-light text-base-content/70">
+                <%= e(option.value, :username, nil) || e(option.value, :character, :username, nil) %>
+              </p>
             </div>
-          </div>
-        <% else %>
-          <div class="flex items-center gap-2">
+          <% end %>
+        </div>
+      </:option>
+
+      <:tag :let={option}>
+        <div class="flex items-center gap-2">
+          <%= if is_binary(option.value) do %>
             <p class="font-semibold text-sm">
               <%= option.label %>
             </p>
-          </div>
-        <% end %>
+          <% else %>
+            <%= if Map.has_key?(debug(option.value), :icon) or Map.has_key?(option.value, "icon") do %>
+              <div class="w-6 h-6">
+                <img src={e(option.value, :icon, nil)} alt="" class="w-full h-full rounded-full" />
+              </div>
+            <% end %>
+            <div class="text-sm">
+              <p class="font-semibold">
+                <%= e(option.value, :name, nil) || e(option.value, :profile, :name, nil) ||
+                  e(option.value, :named, :name, nil) %>
+              </p>
+              <p class="font-light text-base-content/70">
+                <%= e(option.value, :username, nil) || e(option.value, :character, :username, nil) %>
+              </p>
+            </div>
+          <% end %>
+        </div>
       </:tag>
     </LiveSelect.live_select>
     """
