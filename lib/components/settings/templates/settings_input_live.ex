2b0e4a67e28d0fb7df7d76008fb9240d7e41255c
ivanminutillo <ivanminutillo@outlook.it>
2023-08-29 11:20:51 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/686

---
+++ lib/components/settings/templates/settings_input_live.ex
@@ -0,0 +1,25 @@
+defmodule Bonfire.UI.Common.SettingsInputLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop keys, :any, required: true
+  prop default_value, :number, default: 0
+
+  prop name, :string, required: true
+  prop description, :string, default: nil
+  prop placeholder, :string, default: ""
+
+  prop scope, :any, default: nil
+
+  prop read_only, :boolean, default: false
+
+  prop class, :css_class, default: "input input-sm w-16"
+
+  prop current_value, :any, default: :load_from_settings
+  prop input, :string, default: nil
+
+  def render(assigns) do
+    assigns
+    |> Bonfire.Me.Settings.LiveHandler.maybe_assign_input_value_from_keys()
+    |> render_sface()
+  end
+end
