8d19508bc1e9e0183034383e81eb31f7bc707a2f
ivanminutillo <ivanminutillo@outlook.it>
2023-08-31 17:12:03 +0200

    ui

---
+++ lib/components/settings/templates/settings_toggles_live.ex
@@ -0,0 +1,22 @@
+defmodule Bonfire.UI.Common.SettingsTogglesLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop keys, :any, default: []
+  prop options, :any, default: []
+  prop default_value, :any, default: nil
+
+  prop scope, :any, default: nil
+
+  prop name, :string, default: nil
+  prop description, :string, default: nil
+  prop label, :string, default: nil
+
+  prop current_value, :any, default: :load_from_settings
+  prop input, :string, default: nil
+
+  def render(assigns) do
+    assigns
+    |> Bonfire.Me.Settings.LiveHandler.maybe_assign_input_value_from_keys()
+    |> render_sface()
+  end
+end
