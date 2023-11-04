4761c9a987554d880350d83c5c04c9c57e1ce4ed
Mayel de Borniol <git@mayel.space>
2023-11-04 13:36:37 +0000

    canonical_url

--- lib/layout/layout_view.ex
+++ lib/layout/layout_view.ex
@@ -69,7 +69,13 @@ defmodule Bonfire.UI.Common.LayoutView do
       <head>
         <meta charset="utf-8" />
 
-        <Bonfire.UI.Common.SEO.juice item={assigns[:seo] || []} page_title={assigns[:page_title]} />
+        <Bonfire.UI.Common.SEO.juice
+          item={
+            assigns[:seo] || []
+            # TODO: |> Enum.into([canonical_url: assigns[:canonical_url]])
+          }
+          page_title={assigns[:page_title]}
+        />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
