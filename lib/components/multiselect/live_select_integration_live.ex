ddbdaac2e6d5680a02f7301cc4ae716033d05a32
ivanminutillo <ivanminutillo@outlook.it>
2023-11-18 11:37:08 +0100

    daisy

--- lib/components/multiselect/live_select_integration_live.ex
+++ lib/components/multiselect/live_select_integration_live.ex
@@ -23,7 +23,7 @@ defmodule Bonfire.UI.Common.LiveSelectIntegrationLive do
       option_extra_class="rounded-box px-4 py-2"
       tag_class="text-sm badge badge-outline badge-lg !border !border-base-content/30 gap-2 !h-[2rem]"
       dropdown_class="cursor-pointer absolute top-12 p-1 z-[999999999999] dropdown-content menu shadow w-full bg-base-200 shadow-lg text-base-content rounded"
-      tags_container_class="flex flex-wrap gap-1 pt-2 order-last"
+      tags_container_class="flex flex-wrap gap-1 pt-2 order-last px-2"
     >
       <:option :let={option}>
         <div class="flex gap-2 items-center">
