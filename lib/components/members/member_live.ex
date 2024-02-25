d479daaf7723cb066c3234ac8038dd34085879c9
Mayel de Borniol <git@mayel.space>
2024-02-25 22:40:47 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/863

--- lib/components/members/member_live.ex
+++ lib/components/members/member_live.ex
@@ -2,5 +2,6 @@ defmodule Bonfire.UI.Common.MemberLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop user, :map, required: true
+  prop is_local_user, :any, default: nil
   prop counter, :string, default: 0
 end
--- lib/components/members/members_live.ex
+++ lib/components/members/members_live.ex
@@ -2,4 +2,5 @@ defmodule Bonfire.UI.Common.MembersLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop users, :list, required: true
+  prop is_local_users, :any, default: nil
 end
