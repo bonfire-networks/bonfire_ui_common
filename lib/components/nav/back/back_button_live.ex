a09243c87855f53bc44272bcb76655795be4bb62
Mayel de Borniol <git@mayel.space>
2024-03-12 14:18:42 +0000

    misc

--- lib/components/nav/back/back_button_live.ex
+++ lib/components/nav/back/back_button_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.Common.BackButtonLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop close_preview, :any, default: nil
+  prop type, :any, default: nil
   prop back, :any, default: true
 end
