2f68b6ab48750803a82d053118a6658ac76fbf4f
Mayel de Borniol <git@mayel.space>
2024-04-14 22:16:15 +0100

    fix

--- lib/layout/layout_live.ex
+++ lib/layout/layout_live.ex
@@ -107,8 +107,8 @@ defmodule Bonfire.UI.Common.LayoutLive do
         Bonfire.Common.ExtensionModule.default_nav(
           e(assigns[:__context__], :current_extension, nil) ||
             e(assigns[:__context__], :current_app, nil)
-        ) || Bonfire.UI.Common.NavModule.nav(e(assigns[:__context__], :current_app, nil)) || []
-      )
+        ) || Bonfire.UI.Common.NavModule.nav(e(assigns[:__context__], :current_app, nil))
+      ) || []
     )
 
     # |> assign_new(:hero, fn -> nil end)
