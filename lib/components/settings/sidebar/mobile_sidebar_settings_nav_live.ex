33d47a4a8791d5fa67f119ac7a3ab950a0785c67
Mayel de Borniol <git@mayel.space>
2024-07-22 09:50:59 +0100

    move settings nav to ui_me

--- lib/components/settings/sidebar/mobile_sidebar_settings_nav_live.ex
+++
@@ -1,8 +0,0 @@
-defmodule Bonfire.UI.Common.MobileSidebarSettingsNavLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop page, :string, default: nil
-  prop selected_tab, :any, default: nil
-  prop id, :string, default: nil
-  prop showing_within, :atom, default: :settings
-end
