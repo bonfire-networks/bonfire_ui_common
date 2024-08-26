57f63db48f622e81c6e3977c5ed43404a5bf0e81
Mayel de Borniol <git@mayel.space>
2024-08-26 10:12:13 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/910

--- lib/components/extensions/extension_toggle_live.ex
+++ lib/components/extensions/extension_toggle_live.ex
@@ -4,6 +4,7 @@ defmodule Bonfire.UI.Common.ExtensionToggleLive do
   # prop extension, :any, required: true
   prop scope, :any, default: nil
   prop can_instance_wide, :boolean, default: false
+  prop show_explanation, :boolean, default: false
 
   def update(assigns, socket) do
     socket =
@@ -15,13 +16,16 @@ defmodule Bonfire.UI.Common.ExtensionToggleLive do
     global_modularity = Config.get(:modularity, nil, extension)
     # |> debug(extension)
 
+    my_modularity =
+      Settings.get(:modularity, nil, otp_app: extension, context: socket.assigns[:__context__])
+
     {:ok,
      socket
      |> assign(
-       my_modularity:
-         Settings.get(:modularity, nil, otp_app: extension, context: socket.assigns[:__context__]),
-       global_modularity: global_modularity,
-       globally_disabled: Bonfire.Common.Extend.disabled_value?(global_modularity)
+       #  my_modularity: my_modularity,
+       my_disabled?: Bonfire.Common.Extend.disabled_value?(my_modularity),
+       #  global_modularity: global_modularity,
+       globally_disabled?: Bonfire.Common.Extend.disabled_value?(global_modularity)
      )}
   end
 end
