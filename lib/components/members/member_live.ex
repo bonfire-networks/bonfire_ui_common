f82bfc91c7d9ea7abf36b3c7cfb97be02309bc04
Mayel de Borniol <git@mayel.space>
2024-10-14 18:55:45 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1038

--- lib/components/members/member_live.ex
+++ lib/components/members/member_live.ex
@@ -1,5 +1,6 @@
 defmodule Bonfire.UI.Common.MemberLive do
   use Bonfire.UI.Common.Web, :stateless_component
+  # TODO: move to UI.Me
 
   prop user, :map, required: true
   prop is_local_user, :any, default: nil
--- lib/components/members/members_live.ex
+++ lib/components/members/members_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.Common.MembersLive do
   use Bonfire.UI.Common.Web, :stateless_component
-
+  # TODO: move to UI.Me
   prop users, :list, required: true
   prop is_local_users, :any, default: nil
 end
