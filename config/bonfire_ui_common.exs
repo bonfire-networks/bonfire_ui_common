1dbbf89bcc99896af13b3b3e2bf83dd932cdbeec
Mayel de Borniol <git@mayel.space>
2024-11-08 19:15:05 +0000

    config cleanup

--- config/bonfire_ui_common.exs
+++ config/bonfire_ui_common.exs
@@ -1,8 +1,10 @@
 import Config
 
+config :bonfire_common,
+  localisation_path: "priv/localisation"
+
 config :bonfire_ui_common,
-  otp_app: :bonfire,
-  localisation_path: "priv/localisation",
+  otp_app: :bonfire_ui_common,
   default_web_namespace: Bonfire.UI.Common
 
 config :phoenix, :json_library, Jason
