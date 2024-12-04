b70ff1d7c7914db80c258713be21a0d39735b7b4
Mayel de Borniol <git@mayel.space>
2024-12-04 20:36:37 +0000

    fix

--- lib/error/error_live.ex
+++ lib/error/error_live.ex
@@ -12,7 +12,7 @@ defmodule Bonfire.UI.Common.ErrorLive do
   def render(assigns) do
     ~H"""
     <h1>Error</h1>
-    <.live_component id="error" module={Bonfire.UI.Common.ErrorComponentLive} />
+    <.live_component id="error" module={maybe_component(Bonfire.UI.Common.ErrorComponentLive)} />
     """
   end
 end
