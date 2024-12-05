a2c6288395b6362aadcf6b8f7ca0bf5578815b90
Mayel de Borniol <git@mayel.space>
2024-12-05 16:13:12 +0000

    f

--- lib/components/widgets/widget_live.ex
+++ lib/components/widgets/widget_live.ex
@@ -33,22 +33,23 @@ defmodule Bonfire.UI.Common.WidgetLive do
         assigns
 
       module =
-         Extend.maybe_module(widget[:module], assigns[:__context__])
-         |> debug("!maybe_module #{inspect(widget[:module])}") ->
-      data =
-        Enum.into(assigns[:data] || e(widget, :data, nil) || [], assigns[:extra_data] || %{})
-        |> debug("widdata")
+          Extend.maybe_module(widget[:module], assigns[:__context__])
+          |> debug("!maybe_module #{inspect(widget[:module])}") ->
+        data =
+          Enum.into(assigns[:data] || e(widget, :data, nil) || [], assigns[:extra_data] || %{})
+          |> debug("widdata")
 
-      assigns
-      |> assign(
-        widget: Map.merge(widget, %{module: module}),
-        data: data
-      )
-    true ->
-      debug(widget[:module], "disabled")
+        assigns
+        |> assign(
+          widget: Map.merge(widget, %{module: module}),
+          data: data
+        )
 
-      assigns
-      |> assign(widget: %{type: :disabled, name: widget[:module]})
+      true ->
+        debug(widget[:module], "disabled")
+
+        assigns
+        |> assign(widget: %{type: :disabled, name: widget[:module]})
     end
     |> render_sface()
   end
