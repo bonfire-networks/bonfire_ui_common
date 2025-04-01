37817d9f5ff5d0b9d152c87ddcdefdd2985748e0
ivan <ivanminutillo@outlook.it>
2025-04-01 13:39:06 +0200

    Update core_components.ex

--- lib/components/core_components.ex
+++ lib/components/core_components.ex
@@ -427,10 +427,12 @@ defmodule Bonfire.UI.Common.CoreComponents do
 
   def error_msg(assigns) do
     ~H"""
-    <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600">
-      <.iconify icon="heroicons-exclamation-circle-mini" class="mt-0.5 h-5 w-5 flex-none" />
-      {render_slot(@inner_block)}
-    </p>
+    <div role="alert" class="alert alert-warning">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+      </svg>
+      <span>{render_slot(@inner_block)}</span>
+    </div>
     """
   end
 
