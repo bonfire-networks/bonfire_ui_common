d473a2a0b898824b74c12d219ae0d9630ee5a283
ivan <ivanminutillo@outlook.it>
2025-04-17 15:44:05 +0200

    Update widget_text_live.ex

--- lib/components/widgets/widget_text/widget_text_live.ex
+++ lib/components/widgets/widget_text/widget_text_live.ex
@@ -3,7 +3,7 @@ defmodule Bonfire.UI.Common.WidgetTextLive do
 
   prop text, :string, default: nil
   prop widget_title, :string, default: nil
-  prop class, :css_class, default: "max-w-full p-6 prose"
+  prop class, :css_class, default: "max-w-full p-4 prose"
   prop banner_image, :any, default: nil
   prop info, :map, default: nil
 end
