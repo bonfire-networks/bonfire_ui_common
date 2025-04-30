25596339284cf86b7dd7872e74123051478bedaf
Mayel de Borniol <git@mayel.space>
2025-04-30 23:27:05 +0100

    f

--- lib/endpoint_live_reload.ex
+++ lib/endpoint_live_reload.ex
@@ -14,25 +14,25 @@ defmodule Bonfire.UI.Common.Endpoint.LiveReload do
       def halt_live_reload(conn, _), do: conn
 
       if unquote(code_reloading?) && Application.compile_env(:bonfire, :hot_code_reload) &&
-             Code.ensure_loaded?(Phoenix.LiveReloader.Socket) do
-          socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+           Code.ensure_loaded?(Phoenix.LiveReloader.Socket) do
+        socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
 
-          plug(Phoenix.LiveReloader)
-          plug(Phoenix.CodeReloader)
+        plug(Phoenix.LiveReloader)
+        plug(Phoenix.CodeReloader)
 
-          if unquote(System.get_env("WITH_LV_NATIVE") in ["1", "true"]) do
-            plug LiveViewNative.LiveReloader
-          end
-
-          plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :bonfire)
-
-          # FIXME
-          # socket "/admin/system/wobserver", Wobserver.Web.PhoenixSocket
-
-          # plug(PhoenixProfiler)
-        else
-          plug(:halt_live_reload)
+        if unquote(System.get_env("WITH_LV_NATIVE") in ["1", "true"]) do
+          plug LiveViewNative.LiveReloader
         end
+
+        plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :bonfire)
+
+        # FIXME
+        # socket "/admin/system/wobserver", Wobserver.Web.PhoenixSocket
+
+        # plug(PhoenixProfiler)
+      else
+        plug(:halt_live_reload)
+      end
     end
   end
 end
