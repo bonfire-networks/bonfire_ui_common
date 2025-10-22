bf9b896bcc9de466866a747a02f95cb51e2600bc
ivan <ivanminutillo@outlook.it>
2025-10-22 21:29:03 +0200

    smartimput header ui and lazyimage test

--- lib/components/images/avatar_live.ex
+++ lib/components/images/avatar_live.ex
@@ -19,6 +19,7 @@ defmodule Bonfire.UI.Common.AvatarLive do
   prop fallback_class, :css_class, default: "w-5 h-5 opacity-50"
   prop title, :string, default: ""
   prop opts, :any, default: %{}
+  prop disable_lazy, :boolean, default: false
 
   # def classes(%{class: class}) when not is_nil(class) do
   #   class
