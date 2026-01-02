608f705d8610a69f842d14276f753a0b9b3b7e96
ivan <ivanminutillo@outlook.it>
2026-01-02 12:45:57 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1726

--- lib/components/widgets/widget_block_live.ex
+++ lib/components/widgets/widget_block_live.ex
@@ -6,7 +6,7 @@ defmodule Bonfire.UI.Common.WidgetBlockLive do
   prop class, :css_class,
     default: "w-full p-3 flex-auto mx-auto rounded-xl border border-base-content/10"
 
-  prop title_class, :css_class, default: "pb-3 text-sm font-bold tracking-wide"
+  prop title_class, :css_class, default: "pb-3 text-lg font-bold tracking-wide"
 
   @doc "A call to action, usually redirect to the specific page"
   slot action
