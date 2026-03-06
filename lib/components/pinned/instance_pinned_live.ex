b4d2a7d2502f2c608102263162b370169fafe97f
ivan <ivanminutillo@outlook.it>
2026-03-06 16:32:05 +0100

    pin

--- lib/components/pinned/instance_pinned_live.ex
+++ lib/components/pinned/instance_pinned_live.ex
@@ -1,7 +1,29 @@
 defmodule Bonfire.UI.Common.InstancePinnedLive do
-  use Bonfire.UI.Common.Web, :stateless_component
+  use Bonfire.UI.Common.Web, :stateful_component
 
   prop title, :string, default: nil
   prop object_types, :any, default: []
-  prop load_pointer, :boolean, default: false
+
+  def update(assigns, socket) do
+    socket = assign(socket, assigns)
+
+    entries =
+      if socket_connected?(socket) do
+        pinned_activities()
+      else
+        []
+      end
+
+    {:ok, assign(socket, entries: entries)}
+  end
+
+  defp pinned_activities do
+    case Bonfire.Social.Pins.list_instance_pins_activities(
+           paginate?: false,
+           preload: [:feed_by_subject, :feed_postload]
+         ) do
+      %{edges: [_ | _] = edges} -> edges
+      _ -> []
+    end
+  end
 end
