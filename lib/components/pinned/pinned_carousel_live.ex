87fec470f275f98529d02f61a08dc27889057a79
Mayel de Borniol <git@mayel.space>
2026-06-21 20:39:13 +0100

    mv

--- lib/components/pinned/pinned_carousel_live.ex
+++
@@ -1,7 +0,0 @@
-defmodule Bonfire.UI.Common.PinnedCarouselLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop pins, :list, default: []
-  prop title, :string, default: ""
-  # prop object_types, :any, default: []
-end
