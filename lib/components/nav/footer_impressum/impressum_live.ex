f93e57ef6993d715fad120bab2c457d371ced703
ivan <ivanminutillo@outlook.it>
2026-07-10 17:36:22 +0200

    theme

--- lib/components/nav/footer_impressum/impressum_live.ex
+++ lib/components/nav/footer_impressum/impressum_live.ex
@@ -2,4 +2,12 @@ defmodule Bonfire.UI.Common.ImpressumLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop links, :list, default: nil
+
+  def powered_by_name do
+    Config.get([:ui, :theme, :powered_by, :name]) || Bonfire.Application.name_and_flavour()
+  end
+
+  def powered_by_url do
+    Config.get([:ui, :theme, :powered_by, :url]) || "https://bonfirenetworks.org/"
+  end
 end
