8e7ffb3c3d4acc6d8a24c224bf1cc5883cca3bf7
ivan <ivanminutillo@outlook.it>
2026-09-17 13:58:48 +0200

    Update caret_icons_test.exs

--- test/components/caret_icons_test.exs
+++ test/components/caret_icons_test.exs
@@ -1,5 +1,17 @@
 defmodule Bonfire.UI.Common.CaretIconsTest do
   use ExUnit.Case, async: true
 
-  doctest Iconify, only: [outline_caret_icon: 2]
+  doctest Iconify, only: [resolve_icon_weight: 4]
+
+  test "Bonfire excludes filled caret weights while preserving other icons" do
+    for direction <- ~w(right left down up), weight <- ~w(fill duotone) do
+      icon = "caret-#{direction}-#{weight}"
+      assert Iconify.resolve_icon_weight("ph", icon, %{}) == icon
+      assert Iconify.resolve_icon_weight("ph", icon) == "caret-#{direction}"
+    end
+
+    assert Iconify.resolve_icon_weight("ph", "caret-right-bold") == "caret-right-bold"
+    assert Iconify.resolve_icon_weight("ph", "heart-fill") == "heart-fill"
+    assert Iconify.resolve_icon_weight("other", "caret-right-fill") == "caret-right-fill"
+  end
 end
