3f0dd91b8b91e2af802d2a1e2b646916d96fa74f
ivan <ivanminutillo@outlook.it>
2026-09-24 10:19:57 +0200

    icons

--- lib/components/nav/back/back_button_live.ex
+++ lib/components/nav/back/back_button_live.ex
@@ -3,5 +3,7 @@ defmodule Bonfire.UI.Common.BackButtonLive do
 
   prop type, :any, default: nil
   prop back, :any, default: nil
+  prop icon, :string, default: "carbon:chevron-left"
+  prop icon_class, :css_class, default: "w-4 h-4"
   prop class, :css_class, default: "btn btn-xs btn-circle z-50 btn-ghost"
 end
